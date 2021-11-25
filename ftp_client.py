07872ef92396d9eecf8ce6ee8d43d69f33c50df5
danielfts <d.trivino@uniandes.edu.co>
2021-11-25 13:19:41 -0500

    ftp download working

--- ftp_client.py
+++ ftp_client.py
@@ -1,9 +1,28 @@
 from ftplib import FTP
+import os
 host = 'iele1400.ddns.net'
 user = 'c_iele1400'
 passwd = 'uniandes2020'
-ftp = FTP( 'localhost',user, passwd)
 
-print(ftp.retrlines('LIST'))
+def callback(data):
+    print(data)
+    f.write(data)
 
-ftp.quit()
+def download(filename):
+    try:
+        ftp = FTP( host,user, passwd)
+        if ftp:
+            global f
+            f = open(filename, "wb")
+        print(ftp.retrlines('LIST imagenes_proyecto_final'))
+        ftp.cwd("imagenes_proyecto_final")
+        ftp.retrbinary("RETR {}".format(filename), callback)
+        ftp.quit()
+        f.close()
+    except os.error as e:
+        print(e)
+        ftp.quit()
+        f.close()
+
+download("800k.jpg")
+
