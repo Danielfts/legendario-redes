e89215d23e5013c3948ceb7f42bea44689add01a
Danielfts <d.trivino@uniandes.edu.co>
2021-11-26 21:18:05 -0500

    added ping feature

--- ftp_client.py
+++ ftp_client.py
@@ -1,23 +1,32 @@
 from ftplib import FTP
 from ftplib import all_errors
-import os
+from pythonping import ping
 host = 'iele1400.ddns.net'
 user = 'c_iele1400'
 passwd = 'uniandes2020'
 
 def callback(data):
-    print(data)
+    # print(data)
     f.write(data)
+
+    p = ping(host, count = 1)
+    for i in p: 
+        i = str(i).split()[6][:-2]
+        print(i)
+        csv.write(i+'\n')
 
 def download(filename):
     try:
         ftp = FTP( host,user, passwd)
         if ftp:
+            global csv
             global f
+            csv = open('ftp_{}_ping.csv'.format(filename), 'w')
             f = open("ftp_{}".format(filename), "wb")
         print(ftp.retrlines('LIST imagenes_proyecto_final'))
         ftp.cwd("imagenes_proyecto_final")
         ftp.retrbinary("RETR {}".format(filename), callback)
+        csv.close()
         ftp.quit()
         f.close()
     except all_errors as e:
