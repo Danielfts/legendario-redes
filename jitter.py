3bc3838cb3c3471f26e75e2ffd822803c5f986c8
Danielfts <d.trivino@uniandes.edu.co>
2021-12-04 23:33:59 -0500

    built jitter functionality

---
+++ jitter.py
@@ -0,0 +1,22 @@
+path = input('Ingrese la ruta del archivo csv:\n')
+
+f = open(path)
+l = []
+p = [45 ,58, 130, 65] 
+delta = 0
+n = 0
+m = 0
+
+for i in f:
+    l.append(float(i))
+    # print(i)
+# l = p
+for j in range(len(l)-1):
+    d = abs(l[j+1]-l[j])
+    # print(d)
+    delta += d
+
+# print(l)
+# print(delta)
+jitter = delta / (len(l)-1)
+print(jitter)
