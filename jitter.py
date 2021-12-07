6003da648788c29cf7c66ad9ff7497f4d240073f
danielfts <d.trivino@uniandes.edu.co>
2021-12-06 20:12:59 -0500

    cleaned up

--- jitter.py
+++ jitter.py
@@ -1,16 +1,12 @@
 path = input('Ingrese la ruta del archivo csv:\n')
-
 f = open(path)
 l = []
 delta = 0
-
 for i in f:
     l.append(float(i))
-
 for j in range(len(l)-1):
     d = l[j+1]-l[j]
     # print(d)
     delta += d
-
 jitter = delta / (len(l)-1)
 print(jitter)
